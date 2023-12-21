4cf3e81f04cc288f4d2a66de7863f188be2dd1b5
Marko Krstic <mailme@markokrstic.com>
2023-12-21 21:59:08 +0100

    added dropdown

--- edd-bulk-order.php
+++ edd-bulk-order.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: EDD Bulk Order
  * Description: A plugin to submit multiple orders at once for Easy Digital Downloads.
@@ -12,34 +13,71 @@ if (!defined('ABSPATH')) {
 
 add_action('admin_menu', 'edd_bulk_order_menu');
 
-function edd_bulk_order_menu() {
+function edd_bulk_order_menu()
+{
     add_menu_page('EDD Bulk Order', 'EDD Bulk Order', 'manage_options', 'edd-bulk-order', 'edd_bulk_order_page');
 }
 
-function edd_bulk_order_page() {
-    ?>
+function edd_bulk_order_page()
+{
+    // Fetch all downloads using WP_Query
+    $args = array(
+        'post_type'      => 'download',
+        'posts_per_page' => -1  // Retrieve all downloads
+    );
+    $download_query = new WP_Query($args);
+
+?>
+    <style>
+        #email_addresses {
+            min-width: 500px;
+        }
+    </style>
+
     <div class="wrap">
         <h2>EDD Bulk Order</h2>
         <form method="post" action="">
             <?php wp_nonce_field('edd_bulk_order_nonce', 'edd_bulk_order_nonce_field'); ?>
             <p>
-                <label for="product_id">Product ID:</label>
-                <input type="text" name="product_id" id="product_id" />
+                <label for="product_id">Select Download:</label>
             </p>
+            <p>
+                <select name="product_id" id="product_id">
+                    <option value="">-- Select a Download --</option>
+                    <?php
+                    if ($download_query->have_posts()) :
+                        while ($download_query->have_posts()) : $download_query->the_post();
+                    ?>
+                            <option value="<?php echo get_the_ID(); ?>"><?php the_title(); ?></option>
+                    <?php
+                        endwhile;
+                    endif;
+                    wp_reset_postdata();
+                    ?>
+                </select>
+            </p>
+            <br>
             <p>
                 <label for="email_addresses">Email Addresses (one per line):</label>
-                <textarea name="email_addresses" id="email_addresses" rows="10"></textarea>
             </p>
             <p>
-                <input type="submit" value="Submit Orders" class="button-primary"/>
+                <textarea name="email_addresses" id="email_addresses" rows="10"></textarea>
+            </p>
+            <br>
+            <p>
+                <input type="submit" value="Submit Orders" class="button-primary" />
             </p>
         </form>
     </div>
-    <?php
+<?php
     edd_handle_bulk_order_submission();
 }
 
-function edd_handle_bulk_order_submission() {
+
+
+
+function edd_handle_bulk_order_submission()
+{
     if (!isset($_POST['edd_bulk_order_nonce_field']) || !wp_verify_nonce($_POST['edd_bulk_order_nonce_field'], 'edd_bulk_order_nonce')) {
         return;
     }
@@ -56,7 +94,8 @@ function edd_handle_bulk_order_submission() {
     }
 }
 
-function edd_create_order_for_email($product_id, $email) {
+function edd_create_order_for_email($product_id, $email)
+{
     $payment_data = array(
         'price'        => edd_get_download_price($product_id),
         'date'         => date('Y-m-d H:i:s'),
